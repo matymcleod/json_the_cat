4c7d9f29900882a0ced5dc6f42d8767c23d85e86
Mathew <matymcleod@gmail.com>
2021-06-07 19:43:34 +0000

    This might be my forst commit for json the cat, got most of it but was able to completes with help from Ali Bas: mentor

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+
+const fetchBreed = function (breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+  
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      callback('No result found', null);
+      return;
+    }
+
+    if (err) {
+    console.log('API error', err.message);
+    return;
+    
+    }
+      callback(null, data[0].description);
+  });
+};
+
+module.exports = fetchBreed;
