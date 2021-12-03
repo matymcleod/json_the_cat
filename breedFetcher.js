9e15647cee543fd3dde325ba26367275df468905
matymcleod <matymcleod@gmail.com>
2021-12-02 17:01:55 -0800

    some refactoring of breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
   
     const data = JSON.parse(body);
     
     if (data.length === 0) {
-      callback('No input detected', null);
+      callback('No input detected :: ', null);
       return;
     }
 
     if (err) {
-      console.log('API error', err.message);
+      console.log('API error :: ', err.message);
       return;
     
     }
